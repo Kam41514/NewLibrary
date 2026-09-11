5ac7bf9c81d8c072316f4b91116d029119f1c309
Kam41514 <slmbnekamil@gmail.com>
2026-09-11 19:57:10 +0300

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -676,7 +676,6 @@ function ThemeManager:Init(Library, Folder)
     if Library then
         self:SetLibrary(Library)
 
-        -- MoonHub her zaman başlangıç teması olsun.
         self.CurrentTheme = "MoonHub"
 
         local MoonHubTheme = self.Themes.MoonHub
